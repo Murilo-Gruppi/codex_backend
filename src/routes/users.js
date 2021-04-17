d11c7e4cc03d29b3d936edb7483093ddbd4f7622
Murilo <murilo.castro@ccc.ufcg.edu.br>
2021-04-16 23:23:49 -0300

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const UserController = require('../controllers/userController');
 const auth = require('../middleware/auth');
 
-router.get('/', auth, UserController.getUser);
 router.post('/create', UserController.createUser);
 router.post('/login', UserController.userLogin);
 
